b8aeed6725729aa17ea2fc4b7b2d288ed41b8eee
CHRM <limchrm@gmail.com>
2016-08-14 01:35:37 +0900

    워딩 변경, 전원 처치 추가

--- bot.js
+++ bot.js
@@ -20,10 +20,10 @@ controller.spawn({
   token: config.slack_token
 }).startRTM()
 
-controller.hears('done (.*)',['direct_message'],function(bot,message) {
+controller.hears('(.*) 함스터',['direct_message'],function(bot,message) {
   var doneStuff = message.match[1]; //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
   
-  bot.reply(message, doneStuff + ' 했니?');
+  bot.reply(message, doneStuff + ' 했니? :hamster:');
 
  	myFirebaseRef.child('done-stuff').push({
  		text: doneStuff,
@@ -53,33 +53,46 @@ function getItemByIndex(index) {
 }
 
 // give the bot something to listen for.
-controller.hears('(.*) 지워', ['direct_message','direct_mention','mention'], function(bot, message) {
+controller.hears('(.*) 처치', ['direct_message','direct_mention','mention'], function(bot, message) {
   const keyNumber = message.match[1];
+
+  if ( keyNumber == '전원' ) {
+  	myFirebaseRef.child('done-stuff').remove()
+  	  .then(() => {
+      	bot.reply(message, `전원 처치 (+100) :gun::gun::gun:`);
+  	  });
+  	return;
+  }
 
   getItemByIndex(keyNumber - 1)
     .then(item => {
     	return myFirebaseRef.child('done-stuff').child(item.key()).remove()
     	  .then(function () {
     	  	const val = item.val();
-    	  	bot.reply(message, `ㅇㅋ ${val.text} 지움`);
+    	  	bot.reply(message, `${val.text} 처치 (+100) :gun:`);
     	  });
     })
 	  .catch(function(error) {
 	  	bot.reply(message, error.message);
-	  	bot.reply(message, '다시 말해봐~');
 	  });
 });
 
-controller.hears('오바마', ['direct_message','direct_mention','mention'], function(bot, message) {
+controller.hears('람쥐', ['direct_message','direct_mention','mention'], function(bot, message) {
   // Attach an asynchronous callback to read the data at our posts reference
+  bot.reply(message, ':squirrel:')
   myFirebaseRef.child('done-stuff')
     .orderByChild('createdAt')
     .once("value", function(snapshot) {
     	const data = snapshot.val();
+    	if (!data) {
+    		bot.reply(message, '고요를 체험하시오.');
+    		return;
+    	}
+
     	const result = Object.keys(data)
 	    	.map(key => data[key]) // .map(function (key) { return data[key]; })
 	    	.filter(item => item.createdAt) // .filter(function (item) { return item.createdAt; })
-	    	.map((item, index) => `(${index + 1}) ${item.text}`);
+	    	.map((item, index) => `${index + 1}) ${item.text}`);
       bot.reply(message, result.join('\n'));
 	  });
 });
